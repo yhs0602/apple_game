a683ebf42dd58cecece1ae15eb4f3570cb9f04af
Sung Hee <snusnowhite@snu.ac.kr>
2023-07-03 15:17:12 +0900

    Initial Commit

---
+++ AppleGame/AppleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleGameApp.swift
+//  AppleGame
+//
+//  Created by snulife on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct AppleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
